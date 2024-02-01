20898a27f540eabb6b93636d78d663912b7ad074
Okutepa <77410930+Okutepa@users.noreply.github.com>
2024-02-01 13:22:27 -0600

    Added the Javascript
    Added a simple interactivity with Javascript by creating a function to display clicked on this element when the element is clicked on the console.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,54 @@
-console.log("js file is connected");
 
-console.log('id is connected');
+
+/*first SVG JS*/
+const vectorGraphic = document.querySelector('#Reload-icon');
+
+function logThisId() {
+    console.log(this);
+    console.log('clicked on this element', this.id);
+}
+
+vectorGraphic.addEventListener('click', logThisId);
+
+/*first SVG JS*/
+
+const svg2 = document.querySelector('#x-logo');
+
+function logThisId() {
+    console.log(this);
+    console.log('clicked on this element', this.id);
+}
+
+svg2.addEventListener('click', logThisId);
+
+/*third SVG JS*/
+
+const svg3 = document.querySelector('#Wine-time');
+
+function logThisId() {
+    console.log(this);
+    console.log('clicked on this element', this.id);
+}
+
+svg3.addEventListener('click', logThisId);
+
+/*fourth SVG JS*/
+const svg4 = document.querySelector('#Sign_In');
+
+function logThisId() {
+    console.log(this);
+    console.log('clicked on this element', this.id);
+}
+
+svg4.addEventListener('click', logThisId);
+
+/*fifth SVG JS*/
+
+const svg5 = document.querySelector('#zima');
+
+function logThisId() {
+    console.log(this);
+    console.log('clicked on this element', this.id);
+}
+
+svg5.addEventListener('click', logThisId);
